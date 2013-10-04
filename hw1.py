b76be12f9d1a9d7de9529d12229ece9651b16135
Mendelson9 <mendelson9@gmail.cpom>
2013-10-04 15:59:12 -0700

    Problem 1 solved

--- hw1.py
+++ hw1.py
@@ -18,7 +18,16 @@ import math                     # makes the math.sqrt function available
 
 print "Problem 1 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+a = 1
+b = -5.86
+c = 8.5408
+
+root1 = (-b+math.sqrt(b**2-4*a*c))/2*a
+root2 = (-b-math.sqrt(b**2-4*a*c))/2*a
+print root1
+print root2
+
+# root1 fides one root and root2 finds the other.
 
 
 ###
