bf239187a854216160728c90596e761b018ed7d4
Mendelson9 <mennic31@ccm-2610-e3.evergreen.edu>
2013-10-17 14:10:39 -0700

    Fibonacci Sequence
    added formula to caclulate the 'nth' fibonacci number

---
+++ HW3/lab3.py
@@ -0,0 +1,21 @@
+# Name: Nicholas Mendelson
+# Evergreen Login: mennic31
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 3
+
+
+n = 12
+x = 0
+series = 'fibonacci'
+f1 = 1
+f2 = 0
+ans = 0
+if 'fibonacci' == series:
+    for x in range(0,n):
+        
+        ans = f1 + f2
+        f1 = f2
+        f2 = ans
+    print ans
+    
